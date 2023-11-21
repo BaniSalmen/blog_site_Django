b086e570a855f8193731c0304e3050e509556250
salmenbani41 <salmenbani41@gmail.com>
2023-11-21 22:58:08 +0100

    le 21/11/2023

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,71 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User 
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    
+
 # Create your models here.
 class Post(models.Model):
+
+    class Meta:
+        ordering = ['-publish']
+        indexes =[
+            models.Index(fields=['-publish']),
+        ]
+    
+    objects= models.Manager() #the default manager
+    published =PublishedManager() # our custom manager
+    
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
+    
     title = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    class status(models.TextChoices):
+    class Status(models.TextChoices):
         DRAFT = 'DF' ,'DAFT'
         PUBLISHED = 'PB', 'PUBLISHED'
     title=models.CharField(max_length=250)
-    slug=models.SlugField(max_length=250)
+    slug=models.SlugField(max_length=250 , unique_for_date='publish')
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
     status= models.CharField(max_length=2, 
-                             choices=status.choices,
-                             default=status.DRAFT)
+                             choices=Status.choices,
+                             default=Status.DRAFT)
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                             args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])        
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post',
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80, blank=True)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .forms import CommentForm
+
+from .models import Post
+from django.core.paginator import Paginator ,EmptyPage,PageNotAnInteger
+
+
+
 
 # Create your views here.
+
+def post_list(request):
+    post_list=Post.published.all()
+
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,'blog/post/list.html',{'posts':posts})
+
+
+# Create your views here.
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+
+    return render(request,'blog/post/detail.html', {'post': post,
+                                                    'comments':comments ,
+                                                     'form':form })
+
+
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id,status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                           {'post': post,
+                            'form': form,
+                            'comment': comment})
